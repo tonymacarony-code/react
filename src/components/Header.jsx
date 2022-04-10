3db4054c20566b6a5894f8fd4cf2663d67f8bad3
Juli Makarenko <antonybodnev@gmail.com>
2022-04-10 15:33:12 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/4/46/IMG_Academy_Logo.jpg"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { React } from "react";
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="#"></a>
+        Profile
+      </div>
+      <div>
+        <a href="#"></a>
+        Messages
+      </div>
+      <div>
+        <a href="#"></a>
+        News
+      </div>
+      <div>
+        <a href="#"></a>
+        Music
+      </div>
+      <div>
+        <a href="#"></a>
+        Settings
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

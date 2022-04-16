707fb2286b64115863efa85cf2062d5ce26936ca
tonymacarony-code <74680049+tonymacarony-code@users.noreply.github.com>
2022-04-16 17:24:29 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import reportWebVitals from "./reportWebVitals";
-import { rerenderEntireTree } from "./render";
-import state from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/state";
 
-rerenderEntireTree(state);
+import { BrowserRouter } from "react-router-dom";
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <App
+          state={state}
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+        />
+      </React.StrictMode>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+};
+
+// rerenderEntireTree(state);
+
+rerenderEntireTree(store.getState());
+store._subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,59 +1,129 @@
-import { rerenderEntireTree } from "./../render";
-let state = {
-  dialogsPage: {
-    dialogs: [
-      {
-        id: 1,
-        name: "Tony",
-        src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
-      },
-      {
-        id: 2,
-        name: "Aleksi",
-        src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
-      },
-      {
-        id: 3,
-        name: "Yasen",
-        src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
-      },
-      {
-        id: 4,
-        name: "S@nya",
-        src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
-      },
-    ],
-    messages: [
-      { id: 1, message: "Спасибо Леша" },
-      { id: 2, message: "С тебя 5700!" },
-      { id: 3, message: "gre korm suka" },
-      { id: 4, message: "dolboeby blayt'" },
-    ],
+let store = {
+  _state: {
+    dialogsPage: {
+      dialogs: [
+        {
+          id: 1,
+          name: "Tony",
+          src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+        },
+        {
+          id: 2,
+          name: "Aleksi",
+          src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+        },
+        {
+          id: 3,
+          name: "Yasen",
+          src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+        },
+        {
+          id: 4,
+          name: "S@nya",
+          src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+        },
+      ],
+      messages: [
+        { id: 1, message: "Спасибо Леша" },
+        { id: 2, message: "С тебя 5700!" },
+        { id: 3, message: "gre korm suka" },
+        { id: 4, message: "dolboeby blayt'" },
+      ],
+    },
+    profilePage: {
+      posts: [
+        { id: 1, message: "Спасибо Lexa", likesCount: 12 },
+        { id: 2, message: "С тебя 5200!", likesCount: 3 },
+        { id: 3, message: "Yasen" },
+        { id: 4, message: "S@nya" },
+      ],
+      newPostText: "",
+    },
   },
-  profilePage: {
-    posts: [
-      { id: 1, message: "Спасибо Lexa", likesCount: 12 },
-      { id: 2, message: "С тебя 5200!", likesCount: 3 },
-      { id: 3, message: "Yasen" },
-      { id: 4, message: "S@nya" },
-    ],
-    newPostText: "",
+  getState() {
+    return this._state;
+  },
+  _subscriber() {
+    console.log("ok");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._subscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._subscriber(this._state);
+  },
+
+  _subscribe(observer) {
+    this._subscriber = observer;
   },
 };
+export default store;
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
+// let rerenderEntireTree = null;
+// let state = {
+//   dialogsPage: {
+//     dialogs: [
+//       {
+//         id: 1,
+//         name: "Tony",
+//         src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+//       },
+//       {
+//         id: 2,
+//         name: "Aleksi",
+//         src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+//       },
+//       {
+//         id: 3,
+//         name: "Yasen",
+//         src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+//       },
+//       {
+//         id: 4,
+//         name: "S@nya",
+//         src: "https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png",
+//       },
+//     ],
+//     messages: [
+//       { id: 1, message: "Спасибо Леша" },
+//       { id: 2, message: "С тебя 5700!" },
+//       { id: 3, message: "gre korm suka" },
+//       { id: 4, message: "dolboeby blayt'" },
+//     ],
+//   },
+//   profilePage: {
+//     posts: [
+//       { id: 1, message: "Спасибо Lexa", likesCount: 12 },
+//       { id: 2, message: "С тебя 5200!", likesCount: 3 },
+//       { id: 3, message: "Yasen" },
+//       { id: 4, message: "S@nya" },
+//     ],
+//     newPostText: "",
+//   },
+// };
 
-export default state;
+// export const addPost = () => {
+//   let newPost = {
+//     id: 5,
+//     message: state.profilePage.newPostText,
+//     likesCount: 0,
+//   };
+//   state.profilePage.posts.push(newPost);
+//   state.profilePage.newPostText = "";
+//   rerenderEntireTree(state);
+// };
+// export const updateNewPostText = (newText) => {
+//   state.profilePage.newPostText = newText;
+//   rerenderEntireTree(state);
+// };
+// export const subscribe = (observer) => {
+//   rerenderEntireTree = observer;
+// };

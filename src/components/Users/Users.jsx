e8f22dc2299551ea3d5421d96bea00e1c85de71d
Juli Makarenko <antonybodnev@gmail.com>
2022-04-23 19:25:38 +0300

    add server interaction

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,58 @@
 import React, { Component } from "react";
 import * as axios from "axios";
 import userPhoto from "../../assets/img/user.jpg";
-
+import s from "./users.module.css";
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
       .then((response) => {
         this.props.setUsers(response.data.items);
       });
-  }
-
+  };
   render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    let pages = [];
+    for (let i = 1; i < 20; i++) {
+      pages.push(i);
+    }
     return (
       <div>
+        <div>
+          {pages.map((p) => {
+            return (
+              <span
+                className={this.props.currentPage === p && s.selectedPage}
+                onClick={() => {
+                  this.onPageChanged(p);
+                }}
+              >
+                {p}
+              </span>
+            );
+          })}
+        </div>
         {this.props.users.map((u) => (
           <div key={u.id}>
             <span>
               <div>
                 <img
+                  className={s.img}
                   src={u.photos.small != null ? u.photos.small : userPhoto}
                   alt=""
                 />

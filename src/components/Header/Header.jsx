dcc4f91504ae68454a6d6d9888134b0f4e60a399
tonymacarony-code <74680049+tonymacarony-code@users.noreply.github.com>
2022-04-27 16:44:14 +0300

    add header container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
-import { React } from "react";
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Header.module.css";
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img
         src="https://upload.wikimedia.org/wikipedia/en/4/46/IMG_Academy_Logo.jpg"
         alt=""
       />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
